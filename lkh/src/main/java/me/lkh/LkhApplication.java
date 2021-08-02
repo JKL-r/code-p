f5114e44e8756e6eb9923614397bbd9d49c3b26f
이경훈 <leta6603@gmail.com>
2021-08-02 22:22:39 +0900

    make lkh directory?

---
+++ lkh/src/main/java/me/lkh/LkhApplication.java
@@ -0,0 +1,13 @@
+package me.lkh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkhApplication.class, args);
+	}
+
+}
---
+++ lkh/src/test/java/me/lkh/LkhApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lkh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
